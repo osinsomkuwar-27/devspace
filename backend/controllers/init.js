6a79b0f227fde1e2193c249ef2ce2d482a4a4f12
Osin Somkuwar <osinsomkuwar26@gmail.com>
2026-04-01 17:40:47 +0000

    chore: working on the initRepo logic

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,20 @@
+const fs = require("fs").promises;
+const path = require("path");
+
 async function initRepo() {
-    console.log("Backend service initialized.");
+    const repoPath = path.resolve(process.cwd(), ".devspace");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try{
+        await fs.mkdir(repoPath, {recursive: true});
+        await fs.mkdir(commitsPath, {recursive: true});
+        await fs.writeFile(path.join(repoPath, "config.json"),
+        JSON.stringify({bucket: process.env.S3_BUCKET})
+    );
+    console.log("Repository initialised!");
+    }catch(err){
+        console.error("Error initialising repository",err);
+    }
 }
 
 module .exports = {
--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,47 @@ const { hideBin } = require("yargs/helpers");
 
 const { initRepo } = require("./controllers/init");
 const { addRepo } = require("./controllers/add");
+const { commitRepo } = require("./controllers/commit");
+const { pushRepo } = require("./controllers/push");
+const { pullRepo } = require("./controllers/pull");
+const { revertRepo } = require("./controllers/revert");
 
 yargs(hideBin(process.argv))
   .command("init", "Initialize the backend service", {}, initRepo)
-  .command("add <file>", "Add a file to the repository", {}, addRepo)
+  .command(
+    "add <file>",
+    "Add a file to the repository", 
+    (yargs) => { 
+      yargs.positional("file", { 
+        describe: "File to add to the staging area", 
+        type: "string", 
+      }); 
+    }, 
+    addRepo
+  )
+  .command(
+    "commit <message>", 
+    "Commit the staged files", 
+    (yargs) => { 
+      yargs.positional("message", { 
+        describe: "Commit message", 
+        type: "String", 
+      }); 
+    }, 
+    commitRepo
+  )
+  .command("push", "Push commits to S3", {}, pushRepo)
+  .command("pull", "Pull commits from S3", {}, pullRepo)
+  .command(
+    "revert <commitID>",
+    "Revert to a specific commit",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "Commit ID to revert to",
+        type: "string",
+      });
+    },
+    revertRepo
+  )
   .demandCommand(1, "You need atleast one command before moving on")
   .help().argv;
