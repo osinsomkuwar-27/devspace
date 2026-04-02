49fff4fc15490f7035978778a33768b2f51e5c6c
Osin Somkuwar <osinsomkuwar26@gmail.com>
2026-04-02 14:23:59 +0000

    feat: working on the addRepo and commitRepo logic

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ yargs(hideBin(process.argv))
         type: "string", 
       }); 
     }, 
-    addRepo
+    (argv) => {
+      addRepo(argv.file);
+    }
   )
   .command(
     "commit <message>", 
@@ -30,7 +32,9 @@ yargs(hideBin(process.argv))
         type: "String", 
       }); 
     }, 
-    commitRepo
+    (argv) => {
+      commitRepo(argv.message);
+    }
   )
   .command("push", "Push commits to S3", {}, pushRepo)
   .command("pull", "Pull commits from S3", {}, pullRepo)
